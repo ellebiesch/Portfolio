bbd47daf2596f389bd2b0ac5dd2236df89c18f6a
Reymart <31478426+Rcasas14@users.noreply.github.com>
2024-10-09 02:46:48 +0800

    initial commit

---
+++ elvina-portfolio/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMobile: false
+    }
+  },
+  mounted() {
+    this.isMobile = window.innerWidth < 768
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
