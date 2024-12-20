420161e98a4d2bfcb3817179f2af1d991e621c0f
Rcasas14 <reymartcasas123@gmail.com>
2024-12-20 08:07:40 +0800

    update to staging

--- elvina-portfolio/src/components/NavigationElement.vue
+++ elvina-portfolio/src/components/NavigationElement.vue
@@ -6,33 +6,32 @@
         'bg-[#fff]': isScrolled && !isMobile(),
         'bg-[#fff]': isMobile(),
       }"
-      class="fixed w-full z-50 md:px-10 2xl:px-40 border-2 border-[#000]"
+      class="fixed w-full z-50 md:px-10 2xl:px-40"
     >
-      <div class="container hidden lg:block">
-        <div class="container mx-auto flex justify-between items-center h-[120px]" :class="{'hidden': isMobile()}">
-        <!-- Logo -->
-        <div class="logo">
-          <a href="#"><h1 class="elvina-logo font-sans text-2xl font-bold">Elvina.</h1></a>
-        </div>
-        <div class="center-nav flex flex-row">
-          <a href=""><p class="font-inter text-base pr-[52px]">About</p></a>
-          <a href=""><p class="font-inter text-base">Case Studies</p></a>
-        </div>
+      <div class=" container mx-auto flex justify-between items-center lg:block" :class="{'justify-end': isMobile()}">
+          <div class="hidden lg:flex items-center justify-between text-black h-[50px] my-6">
+          <!-- Logo -->
+            <div class="logo">
+              <a href="#"><h1 class="elvina-logo font-sans text-2xl font-bold">Elvina.</h1></a>
+            </div>
+            <div class="center-nav flex flex-row">
+              <a href=""><p class="font-inter text-base pr-[52px]">About</p></a>
+              <a href=""><p class="font-inter text-base">Case Studies</p></a>
+            </div>
 
-        <div class="right-nav flex flex-row items-center">
-          <a href="#"><p class="font-inter text-base pr-[52px]">Download Resume</p></a>
-          <a
-            href="#_"
-            class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none"
-          >
-            Contact
-          </a>
+            <div class="right-nav flex flex-row items-center">
+              <a href="#"><p class="font-inter text-base pr-[52px]">Download Resume</p></a>
+              <a
+                href="#_"
+                class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none"
+              >
+                Contact
+              </a>
+            </div>
         </div>
-      </div>
-
         <!-- button Hamburger -->
-        <div class="lg:hidden md:block">
-            <button class="text-white focus:outline-none mt-1" @click.stop="toggleMobileMenu" >
+        <div class="lg:hidden ">
+            <button class="text-white focus:outline-none mt-1" @click.stop="toggleMobileMenu"  >
               <svg v-show="mobileMenuOpen == false" v-motion-pop-visible  xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="color: #000;">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
@@ -40,7 +39,7 @@
                 <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
               </svg>
             </button>
-          </div>
+        </div>
       </div>
     </nav>
   </div>
--- elvina-portfolio/src/pages/MainPage.vue
+++ elvina-portfolio/src/pages/MainPage.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="container xl:px-[125px]">
-    <div class="flex flex-col just-center align-center">
-      <div class="col-md-8">
-        <!-- <h1 class="container lg:text-7xl xl:font-extrabold lg:font-bold font-inter text-center ">Simplify the interface, amplify the experience</h1> -->
+  <section class="h-auto" >
+    <div id="main" class="container xl:px-[125px] h-screen 2xl:h-auto">
+      <div class="flex flex-col just-center align-center">
+        <div class="col-md-8 font-inter">
+          <h1 class="container lg:text-7xl xl:font-extrabold lg:font-bold text-center ">Simplify the interface, amplify the experience</h1>
+          <p class="text-black text-2xl flex justify-center items-center py-8">Hi, <span> <img src="../assets/wave.png" alt="hand-wave" class="h-full w-[40px]"></span> I am Elvina. I create clear and effective design for seamless user experiences</p>
+        </div>
       </div>
     </div>
-  </div>
+</section>
 </template>
 
 <script>
