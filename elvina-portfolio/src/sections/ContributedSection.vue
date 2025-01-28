0109dcbbbc8bf1f0398cdab44a98a85822c96c9e
Rcasas14 <reymartcasas123@gmail.com>
2025-01-29 07:47:26 +0800

    Update ContributedSection.vue

---
+++ elvina-portfolio/src/sections/ContributedSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="h-auto" >
+      <div class="container">
+        <div class="companies">
+            
+        </div>
+      </div>
+  </section>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style scoped></style>
+  
