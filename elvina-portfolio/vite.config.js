71a5ab7a446ce4e2586b9b00edb39a3e403b2529
Rcasas14 <reymartcasas123@gmail.com>
2025-02-08 07:51:45 +0800

    feat: design process done, todo: mobile responsive

--- elvina-portfolio/vite.config.js
+++ elvina-portfolio/vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __APP_TITLE__: JSON.stringify('elvina'),
+  },
   plugins: [vue()],
   server: {
     port: 5002,
